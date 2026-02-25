eaa06f32ee6f4711f18eb41314a2b46b8e8532dd
Jitesh Kumar <150531216+JITESH-KUMAR05@users.noreply.github.com>
2026-02-25 09:08:57 +0000

    http

---
+++ week-11-12/VulnerableHttpServer.java
@@ -0,0 +1,23 @@
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.InetSocketAddress;
+
+public class VulnerableHttpServer {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
+        server.createContext("/submit", exchange -> {
+            // simulated user input (query string)
+            String query = exchange.getRequestURI().getQuery();
+            //  Example: /submit?msg=Hello
+            String response = "<h2> User Message: " + query + "</h2>"; 
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.start();
+        System.out.println("Vulnerable HTTP Server started on port http://localhost:8000/submit?msg=Hello");
+        
+    }
+}
